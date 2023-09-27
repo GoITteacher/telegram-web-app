3a3940237f9d39594edf089b6778bca7f56a92fb
GoITteacher <50087029+GoITteacher@users.noreply.github.com>
2023-09-27 12:13:16 +0000

    Deploying to gh-pages from @ GoITteacher/telegram-web-app@e0e1c1382b1799b7ec419961c92ab3bc553c0687 🚀

--- index.482cdf21.js
+++
@@ -1,2 +0,0 @@
-var ref,ref1,tg=window.Telegram.WebApp;tg.expand();var spanEl=document.querySelector(".js-name"),avatarEl=document.querySelector(".js-avatar");spanEl.textContent="".concat(null===(ref=tg.initDataUnsafe)||void 0===ref||null===(ref1=ref.user)||void 0===ref1?void 0:ref1.first_name);
-//# sourceMappingURL=index.482cdf21.js.map
